961f0dc595a204c60e3d660d98896e3fe3994364
lou_truong <thanhvantruong.vn@gmail.com>
2025-05-03 18:53:04 +0700

    Sync

--- 2_user_signedin_menu/01_get_user_avater.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (is_user_logged_in()) {
-    $current_user = wp_get_current_user();
-    // Echo avatar HTML tag. Adjust size (e.g., 32) as needed.
-    echo get_avatar($current_user->ID, 32);
-}
--- 2_user_signedin_menu/02_get_user_name.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (is_user_logged_in()) {
-    $current_user = wp_get_current_user();
-    echo esc_html($current_user->display_name);
-    // Or use $current_user->last_name if you prefer
-}
